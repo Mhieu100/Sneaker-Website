2fd6d58ef0a30e009e597bb97c9ab9005a602434
mhieu100 <hieunguyen201103@gmail.com>
2023-04-13 23:23:18 +0700

    1.00.50s

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -5,7 +5,7 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
   return (
     <>
       <div
-        className={`relative bg-gradient-to-b ${color} ${shadow} grid justify-items-center items-center`}
+        className={`relative bg-gradient-to-b ${color} ${shadow} grid justify-items-center items-center rounded-xl px-5 py-4 ease-in-out w-full hover:scale-105`}
       >
         <div className="grid justify-items-center items-center">
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium  filter drop-shadow">
@@ -16,23 +16,27 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
           </p>
 
           <div className="flex justify-between items-center w-28 my-2">
-            <div className="flex items-center bg-white/80 px-1 rounded">
-              <h1 className="text-black text-sm font-medium">{price}</h1>
+            <div className="flex items-center bg-white/80 px-1 rounded blur-effect-theme">
+              <h1 className="text-black text-sm font-medium">${price}</h1>
             </div>
             <div className="flex items-center gap-1">
               <StarIcon className="icon-style w-5 h-5 md:w-4 md:h-4" />
-              <h1 className="md:text-sm font-normal text-slate-100">{rating}</h1>
+              <h1 className="md:text-sm font-normal text-slate-100">
+                {rating}
+              </h1>
             </div>
           </div>
           <div className="flex items-center gap-3">
-            <button className="">
+            <button className="bg-white/90 blur-effect-theme button-theme p-0.5  shadow shadow-sky-200">
               <ShoppingBagIcon className="icon-style text-slate-900" />
             </button>
-            <button>{btn}</button>
+            <button className="bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-sky-200 text-sm text-black">
+              {btn}
+            </button>
           </div>
         </div>
         <div>
-          <img src={img} alt="img/item-img" />
+          <img src={img} alt="img/item-img" className="h-36 w-64 transitions-theme hover:-rotate-12" />
         </div>
       </div>
     </>
