31c8218e144e9d817f1c9d770369c8bed9ff9aec
mhieu100 <hieunguyen201103@gmail.com>
2023-04-13 22:52:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Hero, Sales } from "./components";
+import { heroapi, popularsales, toprateslaes } from "./data/data.js";
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} />
+        <Sales endpoint={toprateslaes} />
+      </main>
+    </>
+  );
+}
+
+export default App
